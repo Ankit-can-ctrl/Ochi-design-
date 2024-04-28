64431271f6cfcee6d80e7f43fa7e25e016cc04d0
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-04-28 18:01:32 +0530

    footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Cards from "./components/Cards";
 import Eyes from "./components/Eyes";
 import Featured from "./components/Featured";
+import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
 import Marquee from "./components/Marquee";
 import Navbar from "./components/Navbar";
@@ -23,6 +24,7 @@ function App() {
       <Review />
       <Cards />
       <ReadyToStart />
+      <Footer />
     </div>
   );
 }
