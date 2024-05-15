29c762258311272847a205d05ea3f7d2a1cdf218
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-05-15 19:47:45 +0530

    rounded work landing

--- src/work/Work.jsx
+++ src/work/Work.jsx
@@ -44,39 +44,41 @@ function Work() {
       <div className="work-nav">
         <NewNavbar />
       </div>
-      <div className="work-eyes h-fit bg-[#d5f269] pt-20 overflow-hidden">
-        <h1 className="text-left font-Neue text-gray-800 px-14 md:text-[150px] leading-loose py-10 text-8xl font-bold tracking-tighter">
-          Work
-        </h1>
-        <div className="eyes relative flex flex-col items-center justify-center">
-          <div className=" flex gap-5 md:gap-10 items-center justify-center">
-            <div className="each-eye-main flex items-center justify-center h-[150px] w-[150px] md:h-[200px] md:w-[200px] rounded-full bg-zinc-100">
-              <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
-                <div
-                  style={{
-                    transform: `translate(-50%,-50%) rotate(${rotate}deg)`,
-                  }}
-                  className=" line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  w-full h-10"
-                >
-                  <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
+      <div className=" bg-black">
+        <div className="work-eyes h-fit rounded-t-3xl bg-[#d5f269] pt-20 overflow-hidden">
+          <h1 className="text-left font-Neue text-gray-800 px-14 md:text-[150px] leading-loose py-10 text-8xl font-bold tracking-tighter">
+            Work
+          </h1>
+          <div className="eyes relative flex flex-col items-center justify-center">
+            <div className=" flex gap-5 md:gap-10 items-center justify-center">
+              <div className="each-eye-main flex items-center justify-center h-[150px] w-[150px] md:h-[200px] md:w-[200px] rounded-full bg-zinc-100">
+                <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
+                  <div
+                    style={{
+                      transform: `translate(-50%,-50%) rotate(${rotate}deg)`,
+                    }}
+                    className=" line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  w-full h-10"
+                  >
+                    <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="each-eye-main flex items-center justify-center h-[150px] w-[150px] md:h-[200px] md:w-[200px] rounded-full bg-zinc-100">
+                <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
+                  <div
+                    style={{
+                      transform: `translate(-50%,-50%) rotate(${rotate}deg)`,
+                    }}
+                    className=" line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  w-full h-10"
+                  >
+                    <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="each-eye-main flex items-center justify-center h-[150px] w-[150px] md:h-[200px] md:w-[200px] rounded-full bg-zinc-100">
-              <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
-                <div
-                  style={{
-                    transform: `translate(-50%,-50%) rotate(${rotate}deg)`,
-                  }}
-                  className=" line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  w-full h-10"
-                >
-                  <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
-                </div>
-              </div>
-            </div>
+            <div className=" absolute bottom-0 top-28 bg-[#a6bf49] rounded-3xl w-full md:w-[80%] h-32"></div>
+            <div className=" absolute bottom-0 top-32 md:top-36 bg-[#96ac3f] rounded-3xl w-full md:w-[90%] h-32"></div>
           </div>
-          <div className=" absolute bottom-0 top-28 bg-[#a6bf49] rounded-3xl w-full md:w-[80%] h-32"></div>
-          <div className=" absolute bottom-0 top-32 md:top-36 bg-[#96ac3f] rounded-3xl w-full md:w-[90%] h-32"></div>
         </div>
       </div>
       <div className="projects  bg-[#96ac3f]">
@@ -132,7 +134,7 @@ function Work() {
         </div>
       </div>
       <div className=" bg-[#004D43]">
-        <div className=" rounded-t-2xl overflow-hidden">
+        <div className=" rounded-t-3xl overflow-hidden">
           <ReadyToStart />
         </div>
       </div>
